a791fd20ae97fa95a232e4fc61f3b7b94fefd99a
cet <cets021@gmail.com>
2023-02-25 16:06:14 +0900

    aa

--- src/my.py
+++ src/my.py
@@ -3,17 +3,22 @@ import pygame
 from pygame.locals import *
 
 
-class mine:
-    def display_init(
-        window_title: str,
-        resolutions: tuple[int, int]
+class _mine:
+    def window_init(
+        _window_title: str,
+        _resolutions: tuple[int, int]
     ) -> pygame.Surface:
         pygame.init()
-        pygame.display.set_caption(window_title)
-        return pygame.display.set_mode(resolutions)
+        pygame.display.set_caption(_window_title)
+        return pygame.display.set_mode(_resolutions)
 
     def app_quit():
+        def q():
+            pygame.quit()
+            sys.exit()
+
         for e in pygame.event.get():
-            if e.type == QUIT:
-                pygame.quit()
-                sys.exit()
+            if e.type == QUIT or (
+                e.type == KEYDOWN and e.type == K_ESCAPE
+            ):
+                q()
