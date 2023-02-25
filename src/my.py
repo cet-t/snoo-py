4768416dc3731de91fbfe7e99319f38eff2a9acc
cet <cets021@gmail.com>
2023-02-25 14:53:56 +0900

    first commit

---
+++ src/my.py
@@ -0,0 +1,19 @@
+﻿import sys
+import pygame
+from pygame.locals import *
+
+
+class mine:
+    def display_init(
+        window_title: str,
+        resolutions: tuple[int, int]
+    ) -> pygame.Surface:
+        pygame.init()
+        pygame.display.set_caption(window_title)
+        return pygame.display.set_mode(resolutions)
+
+    def app_quit():
+        for e in pygame.event.get():
+            if e.type == QUIT:
+                pygame.quit()
+                sys.exit()
---
+++ src/snoo.py
@@ -0,0 +1,46 @@
+﻿import pygame
+from pygame.locals import *
+import sys
+import typing
+# import my as mine
+
+type_rgb = tuple[int, int, int]
+
+# sys.path.append('./my.py')
+
+screen_settings: dict[str, typing.Any] = {
+    'title': 'Snoopy',
+    'size': (1280, 720),
+    'bg_color': (32, 107, 0)
+}
+
+
+class mine:
+    def screen_init(
+        _window_title: str,
+        _resolutions: tuple[int, int]
+    ) -> pygame.Surface:
+        pygame.init()
+        pygame.display.set_caption(_window_title)
+        return pygame.display.set_mode(_resolutions)
+
+    def app_quit():
+        for e in pygame.event.get():
+            if e.type == QUIT or (
+                e.type == KEYDOWN and e.type == K_ESCAPE
+            ):
+                pygame.quit()
+                sys.exit()
+
+
+def main(_background_rgb: type_rgb):
+    screen = mine.screen_init(
+        screen_settings['title'], screen_settings['size'])
+
+    while True:
+        screen.fill(_background_rgb)
+        mine.app_quit()
+
+
+if __name__ == '__main__':
+    main(screen_settings['bg_color'])
